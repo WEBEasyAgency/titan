dd59aeef913431d0afaaa7897c53e5e45c407182
ravencler@gmail.com <ravencler@gmail.com>
2026-03-03 17:37:23 +0700

    Fix

--- wp-content/themes/titan/footer.php
+++ wp-content/themes/titan/footer.php
@@ -26,7 +26,7 @@
 				<div class="copyright-text">
 					<div class="flex">
 						<div class="text">Все права защищены <?php echo esc_html( date( 'Y' ) ); ?></div>
-						<div class="text"><a href="https://docs.google.com/document/d/1jpQdOeP_Fh4LwUrf2g55WpN_QuJsMKIW/edit?usp=sharing&ouid=100779751692423724630&rtpof=true&sd=true" target="_blank">Политика конфиденциальности</a></div>
+						<div class="text"><a href="http://titan.realeasystudio.site/wp-content/uploads/2026/03/dogovor-oferty-i-politika-konf.doc" target="_blank">Политика конфиденциальности</a></div>
 					</div>
 				</div>
 			</div>
